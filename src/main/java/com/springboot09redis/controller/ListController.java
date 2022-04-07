3361f1bda50534d7ae9941bdb90a58a088403f36
ph <ph@tt.com>
2022-04-07 17:13:24 +0800

    第二次提交

---
+++ src/main/java/com/springboot09redis/controller/ListController.java
@@ -0,0 +1,56 @@
+package com.springboot09redis.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.ListOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.util.List;
+
+@Controller
+public class ListController {
+
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
+    @GetMapping("/list")
+    public String list(){
+        return "addlist";
+    }
+
+    @GetMapping("/listAll")
+    public String listAll(Model model){
+        ListOperations<String, String> operations  = redisTemplate.opsForList();
+        List<String> names = operations.range("name", 0, -1);
+        model.addAttribute("listAll",names);
+        return "listAll";
+    }
+
+    @PostMapping("/add")
+    public String add(){
+        return "addlist";
+    }
+
+    @PostMapping("/addList")
+    public String addList(String name , Model model){
+        ListOperations<String, String> operations  = redisTemplate.opsForList();
+        operations.rightPush("name", name);
+        List<String> names = operations.range("name", 0, -1);
+        //SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
+        model.addAttribute("listAll",names);
+        //model.addAttribute("time",df.format(new Date()));
+        return "redirect:/listAll";
+    }
+
+    @GetMapping("/del/{name}")
+    public String delete(@PathVariable("name")String name, Model model){
+        System.out.println(name);
+        ListOperations<String, String> operations  = redisTemplate.opsForList();
+        operations.remove("name",1,name);
+        return "redirect:/listAll";
+    }
+
+}
--- src/test/java/com/springboot09redis/Springboot09RedisApplicationTests.java
+++ src/test/java/com/springboot09redis/Springboot09RedisApplicationTests.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -12,23 +13,23 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.ui.Model;
 
 @SpringBootTest
-@SpringJUnitConfig(classes = RedisConfig.class)
 class Springboot09RedisApplicationTests {
 
     @Autowired
     StringRedisTemplate redisTemplate;
 
-    @Autowired
-    RedisConnectionFactory redisConnectionFactory;
+
 
     @Test
     void testRedis(){
         ValueOperations<String, String> operations = redisTemplate.opsForValue();
-
         operations.set("hello","world");
-
         String hello = operations.get("hello");
         System.out.println(hello);
+        HashOperations<String, Object, Object> operation = redisTemplate.opsForHash();
+        operation.put("user","name","zhangsan");
+        System.out.println(operation.get("user","name"));
+
     }
 
 
